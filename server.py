4db6b5aab64350ec7a26f6fb9aa385f0de4a395f
Baskoibanes <vovazms@gmail.com>
2023-12-06 17:59:11 +0300

    Final program

--- server.py
+++ server.py
@@ -34,6 +34,27 @@ def changeMode():
         mode = request.json['mode']
         ser = serial.Serial(session.get('port'), 115200)
         xdm.xdm_changeMode(ser, mode)
+        ser.close()
+        return "Success", 200
+
+
+@app.route('/make_single_eval/', methods=['POST'])
+def make_single_eval():
+    if request.method == "POST":
+        ser = serial.Serial(session.get('port'), 115200)
+        msg = xdm.make_single_eval(ser)
+        ser.close()
+        return {"msg": msg}
+
+
+@app.route('/changeVarMode/', methods=['POST'])
+def changeVarMode():
+    if request.method == "POST":
+        mode = request.json['mode']
+        varMode = request.json['varMode']
+        ser = serial.Serial(session.get('port'), 115200)
+        xdm.xdm_changeVarMode(ser, mode, varMode)
+        ser.close()
         return "Success", 200
 
 
@@ -42,8 +63,13 @@ def make_eval():
     if request.method == "POST":
         var = request.json['var']
         time_v = float(request.json['time'])
-        dur = float(request.json['dur'])
+        mode = request.json['mode']
+        path = request.json['path']
+        low_int = request.json['low_int']
+        high_int = request.json['high_int']
+        dur = request.json['dur']
         ser = serial.Serial(session.get('port'), 115200)
-        path_csv, c_time = xdm.make_eval_with_duration(ser, var, time_v, dur)
-        path_graph = xdm.make_graph_from_csv(path_csv, c_time)
-        return {'image': path_graph}
+        path, path_graph = xdm.make_eval_with_graph(ser, var, time_v, path, mode, low_int, high_int, dur)
+        ser.close()
+        print(f'..{path_graph[1:]}')
+        return {'image': path_graph[1:], 'path': path}
--- xdm.py
+++ xdm.py
@@ -1,3 +1,5 @@
+import os
+
 import serial
 import time
 import pandas as pd
@@ -12,41 +14,96 @@ matplotlib.use('agg')
 def xdm_changeMode(ser, mode):
     mode = "CONF:" + mode + '\n'
     ser.write(mode.encode('ASCII'))
+    time.sleep(1)
 
-def make_eval_with_duration(ser, var, time_v, dur):
-    mode = "CONF:" + var + '\n'
-    ser.write(mode.encode('ASCII'))
+
+def xdm_changeVarMode(ser, mode, varMode):
+    if mode == "VOLT" or mode == "CURR" :
+        varMode = "CONF:" + mode + ':' + varMode + '\n'
+    elif mode == "RES":
+        varMode = "CONF:" + mode + ' ' + varMode + '\n'
+    else:
+        varMode = mode + ':' + varMode + '\n'
+    print(varMode)
+    ser.write(varMode.encode('ASCII'))
+    time.sleep(1)
+
+
+def make_single_eval(ser):
+    ser.write(b'MEAS?\n')
+    msg = float(ser.readline().decode('ASCII'))
+    return msg
+
+
+def change_mode_and_wait(ser, var):
+    xdm_changeMode(ser, var)
 
     time.sleep(1)
 
-    time_l = []
-    var_l = []
 
-    for i in np.arange(0, dur, time_v):
-        time_l.append(i)
-        ser.write(b'MEAS?\n')
-        msg = float(ser.readline().decode('ASCII'))
-        var_l.append(msg)
-        print(i, msg)
-        time.sleep(time_v)
-    df = pd.DataFrame({'time': time_l, f'{var}': var_l})
-    current_time = time.strftime('%H.%M.%S', time.localtime())
-    path_csv = f'./csv/{current_time}_{var}.csv'
+def initialize_csv(path, df):
+    df.to_csv(path)
+
+
+def make_eval_with_graph(ser, var, time_v, path, mode, low_int, high_int, dur):
+
+    print("Begin evaluation with time ", time_v)
+
+    if time_v == 0.:
+        # xdm_changeVarMode(ser, mode, var)
+        curr_time = time.strftime('%d.%m.%Y___%H_%M_%S', time.localtime())
+        path = f'./static/{curr_time}'
+        if not os.path.exists('./static'):
+            os.mkdir('static')
+        os.mkdir(path)
+
+    msg = make_single_eval(ser)
+    print(time_v, msg)
+
+    evaluation = {"time": [time_v], f'{var}': [msg]}
+
+    if not os.path.exists(f'./{path}/{var}.csv'):
+        df = pd.DataFrame(evaluation)
+
+    else:
+        df = pd.read_csv(f'./{path}/{var}.csv', usecols=['time', f'{var}'])
+        df.loc[len(df.index)] = [time_v, msg]
+
+    path_csv = f'./{path}/{var}.csv'
     df.to_csv(path_csv)
-    return path_csv, current_time
+    path_graph = make_graph_from_csv(path, mode, var, low_int, high_int, dur)
+
+    return path, path_graph
 
 
-def make_graph_from_csv(path_csv, c_time):
-    df = pd.read_csv(path_csv)
+def make_graph_from_csv(path, mode, var, low_int, high_int, dur):
+    df = pd.read_csv(f'{path}/{var}.csv')
 
     x = df.iloc[:, 1]
     y = df.iloc[:, 2]
 
+    plt.figure(figsize=(16, 6))
+
     plt.plot(x, y)
 
+    plt.xlim([0, float(dur)])
+    plt.ylim([float(low_int), float(high_int)])
+
     plt.xlabel("Time, seconds")
-    plt.ylabel(list(df.columns)[2])
-    path_graph = '/static/' + f'{c_time}_' + list(df.columns)[2] + '.png'
-    plt.savefig('.' + path_graph)
+    m_var = list(df.columns)[2]
+    if m_var == "VOLT":
+        plt.ylabel('Voltage, V')
+    elif m_var == "CURR":
+        plt.ylabel('Current, A')
+    elif m_var == "TEMP":
+        print(mode)
+        plt.ylabel('Temperature, ' + mode[-1])
+    elif m_var == "FREQ":
+        plt.ylabel('Frequency, Hz')
+    elif m_var == "RES":
+        plt.ylabel('Resistance, Ω')
+    path_graph = f'{path}/{var}.png'
+    plt.savefig(path_graph)
     plt.close()
     return path_graph
+
